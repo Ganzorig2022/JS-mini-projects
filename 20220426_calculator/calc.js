cb989a97aebff71bee95766f227f73f74bfd34d1
Ganzorig2022 <ganzo.galaxy@gmail.com>
2022-04-27 18:03:58 +0800

    Updated_0427

--- 20220426_calculator/calc.js
+++ 20220426_calculator/calc.js
@@ -2,25 +2,26 @@ const operatorsBtn = document.getElementsByClassName("operator");
 const clear = document.getElementsByClassName("clear")[0];
 let numbers = document.getElementsByClassName("number");
 const equalBtn = document.getElementsByClassName("equal")[0];
-
+const floatBtn = document.getElementsByClassName("float")[0];
 let output = document.getElementsByClassName("output")[0];
+
 let operand1 = 0;
 let operand2 = 0;
 let operation = "";
-// for (let i = 0; i < operators.length; i++) {
-//   operators[i].addEventListener("click", function (event) {
-//     let a = operators[i].textContent;
-//   });
-// }
+
+// .tseg deer darhad display dr
+floatBtn.addEventListener("click", function () {
+  output.textContent += ".";
+});
 
 for (let i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener("click", function (event) {
     //target object ni yag ali towchin der darsniig haruulj ogdog.
     if (output.textContent === "0") {
+      //toonii urd 0 garaad bgag arilgaw.
       output.textContent = event.target.textContent;
-    } else {
-      output.textContent += event.target.textContent;
-    }
+      // daraagiin toog darhad omnoh toon der zalgaj ogow.
+    } else output.textContent += event.target.textContent;
   });
 }
 
@@ -40,17 +41,17 @@ function sub(a, b) {
   return a - b;
 }
 
-//= darhad hariu ni gardag
+// = tentsuu dr darhad hariu ni gardag
 
 equalBtn.addEventListener("click", () => {
   let result = 0;
   operand2 = output.textContent;
   if (operation === "+") {
-    result = add(parseInt(operand1), parseInt(operand2));
+    result = add(parseFloat(operand1), parseFloat(operand2));
   } else if (operation === "-") {
     result = sub(parseInt(operand1), parseInt(operand2));
   }
-  output.textContent = result;
+  output.textContent = result.toFixed(2);
 });
 
 // clear towch dr darhad display dr hariug 0 bolgoj reset-ledeg.
