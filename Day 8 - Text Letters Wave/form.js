2f2597593030caba88ac41a972f699ad094ad874
Ganzorig <103576449+Ganzorig2022@users.noreply.github.com>
2022-04-28 21:20:54 +0800

    Update form.js

--- Day 8 - Text Letters Wave/form.js
+++ Day 8 - Text Letters Wave/form.js
@@ -1,15 +1,17 @@
 const labels = document.querySelectorAll(".form-control label");
+console.log(labels);
 
 labels.forEach((label) => {
   // 1.label tag dotorh EMAIL gesen vgend dund LOOOP hiine.
   label.innerHTML = label.innerText
     // 2.EMAIL gesen vgiig E-M-A-I-L gej neg negeer ni salgana.
     .split("")
-    // 3.Map() function-aar salgaj awsan vsegnvvdeeree array uusgene. Indehdee letter, idx gesen 2 parameter damjuulna. E useg 0 idx dr, M useg 1 idx dr geh met.
+    // 3.Map() function-aar salgaj awsan vsegnvvdeeree array uusgene. Indehdee letter, idx gesen 2 parameter damjuulna. E useg 0 idx dr, M useg 1 idx dr geh met. Utga butsaahda shineer SPAN vvsgej ogow.
     .map(
-      (letter, idx) =>
-        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+      function (letter, idx) {
+        return `<span style="transition-delay:${idx * 50}ms">${letter}</span>`;
+      }
     )
-    //   join() function zaaj ogohgv bol bvh useg E,M,A,I,L gej haragdana. Join() function- ni ARRAY-n elementvvdiig STRING bolgoj RETURN hiideg.
+    //   join() function dotor hooson "" zaaj ogohgv bol bvh useg E,M,A,I,L gej haragdana. Join() function- ni ARRAY-n elementvvdiig STRING bolgoj RETURN hiideg.
     .join("");
 });
