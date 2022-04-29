67c981bef9f8964ffecbe14e28bc9f7dc2fdadaf
Ganzorig <103576449+Ganzorig2022@users.noreply.github.com>
2022-04-29 11:13:59 +0800

    Update app.js

--- XOXOX/app.js
+++ XOXOX/app.js
@@ -1,14 +1,21 @@
 const boxBtn = document.getElementsByClassName("box");
-// // boxBtn.forEach((event) => {
-// //     event.
-// // });
 
+let playerTurn = 0;
+
+
+//Toglogch 1, 1-eeree daraad yawah heseg
 for (let i = 0; i < boxBtn.length; i++) {
-  boxBtn[i].addEventListener("click", function (event) {
-    console.log(boxBtn[i]);
-    // boxBtn[i].classList.add("cirlce");
-    document.getElementsByTagName("span")[i].classList.toggle("circle");
-    // boxBtn[i].childNodes[0].class;
-    // boxBtn[i].children.style.display = "block";
+  boxBtn[i].addEventListener("click", function () {
+    // console.log(boxBtn[i]);
+    console.log(boxBtn[0]);
+    playerTurn++;
+    if (playerTurn % 2 == 0) {
+      document.getElementsByTagName("span")[i].classList.toggle("circle");
+    } else {
+      document.getElementsByTagName("span")[i].classList.toggle("x");
+    }
   });
+
+  //toglogch yalah heseg
 }
+
